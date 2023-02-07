600b4dc60a475d479b40a445b5800b61eb79a234
Sadiq Alaskar <38132089+sadiq1419@users.noreply.github.com>
2023-02-07 15:42:25 +0300

    added sort function

--- Sorter.java
+++ Sorter.java
@@ -1,35 +1,44 @@
 
 public class Sorter {
-	static void swap(int[] array, int a, int b){
+	public static void main(String[] args) {
+		System.out.println("Something");
+	}
+	static void swap(int[] array, int a, int b) {
 		int temp = array[a];
 		array[a] = array[b];
 		array[b] = temp;
 	}
 
-	public static int[] selectionSort(int[] array){
+	public static int[] selectionSort(int[] array) {
 		int min;
-		for (int i=0; i < array.length; i++) {
+		for (int i = 0; i < array.length; i++) {
 			min = i;
-			for (int j=i; j<array.length; j++) {
-				if(array[min] > array[j])
+			for (int j = i; j < array.length; j++) {
+				if (array[min] > array[j])
 					min = j;
 			}
 			swap(array, i, min);
 		}
 		return array;
-}
+	}
 
-public static int[] insertionSort(int[] array){
-	for (int i=1; i<array.length; i++) {
-		for (int j=0; j<i; j++) {
-			if(array[i] < array[j]){
-				for(int k = i; k>j; k--){
-					swap(array, k, k-1);
+	public static int[] insertionSort(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (array[i] < array[j]) {
+					for (int k = i; k > j; k--) {
+						swap(array, k, k - 1);
+					}
+					break;
 				}
-				break;
 			}
 		}
+		return array;
 	}
-	return array;
-}
+
+
+	public static int[] sort(int[] arr){
+		return selectionSort(arr);
+	}
+	
 }
